0b429dba27376d91bde0326d9f4797b27c4caab9
Jorge Villarrubia <georgeatletico@gmail.com>
2019-04-09 18:29:25 +0200

    Add files via upload

--- MochilaVueltaAtras.cpp
+++ MochilaVueltaAtras.cpp
@@ -17,7 +17,6 @@ float const RANDOM_MAX = 5.0;
 size_t const RANDOM_NAT_MIN = 1;
 size_t const RANDOM_NAT_MAX = 5;
 
-
 enum poda {
 	FACTIBILIDAD, OPTIMALIDAD, AMBAS
 };
@@ -29,19 +28,22 @@ struct DatosProblema
 	vector<float> valores;
 };
 
-float cotaSupOpt(vector<int> const& objetos, DatosProblema const& dp, float b, float tam_usado, int indice) {
-	float res = b;
-	float tam_disponible = dp.tam_Mochila - tam_usado;
-	while (indice < objetos.size() && dp.pesos[objetos[indice]] <= tam_disponible) {
-		tam_disponible -= dp.pesos[objetos[indice]];
-		res += dp.valores[objetos[indice]];
-		indice++;
-	}
-	if (indice < objetos.size()) {
-		res += (tam_disponible / dp.pesos[objetos[indice]] * dp.valores[objetos[indice]]);
-	}
 
-	return res;
+
+float cotaSupOpt(vector<int> const& objetos, DatosProblema const& dp, float peso, float beneficio, int k) {
+	float opt;
+	int n = objetos.size();
+	float hueco = dp.tam_Mochila - peso; opt = beneficio;
+	int j = k + 1;
+	while (j <= n && dp.pesos [objetos[j - 1]]<= hueco) {
+		hueco -= dp.pesos[objetos[j - 1]];
+		opt += dp.valores[objetos[j - 1]];
+		j++;
+	}
+	if (j <= n) {
+		opt += opt + (hueco / dp.pesos[objetos[j - 1]]) * dp.valores[objetos[j - 1]];
+	}
+	return opt;
 }
 
 
@@ -81,27 +83,26 @@ void VueltaAtrasFactibilidad(vector<int>const & objetos, DatosProblema const& dp
 }
 
 
-void VueltaAtrasOptimalidad(vector<int>const & objetos, DatosProblema const& dp, float valor_candidato, float peso_candidato, float& sol, int indiceElemento, size_t & nodos) {
+void VueltaAtrasOptimalidad(vector<int>const & objetos, DatosProblema const& dp, float valor_candidato, float peso_momentaneo, float& sol, int indiceElemento, size_t & nodos) {
 
 	nodos++;
 
-	if (valor_candidato + dp.valores[objetos[indiceElemento]] > sol && peso_candidato + dp.pesos[objetos[indiceElemento]] <= dp.tam_Mochila) {
+	if (valor_candidato + dp.valores[objetos[indiceElemento]] > sol && peso_momentaneo + dp.pesos[objetos[indiceElemento]] <= dp.tam_Mochila) {
 		sol = valor_candidato + dp.valores[objetos[indiceElemento]];
 	}
 
 	if (indiceElemento + 1 < objetos.size()) {
 		// Marcado y desmarcado
-		if (peso_candidato + dp.pesos[objetos[indiceElemento]] <= dp.tam_Mochila) {
+		if (peso_momentaneo + dp.pesos[objetos[indiceElemento]] <= dp.tam_Mochila) {
 			valor_candidato += dp.valores[objetos[indiceElemento]];
-			peso_candidato += dp.pesos[objetos[indiceElemento]];
-			if (peso_candidato + dp.pesos[objetos[indiceElemento + 1]] <= dp.tam_Mochila)
-				VueltaAtrasFactibilidad(objetos, dp, valor_candidato, peso_candidato, sol, indiceElemento + 1, nodos);
+			peso_momentaneo += dp.pesos[objetos[indiceElemento]];
+			VueltaAtrasOptimalidad(objetos, dp, valor_candidato, peso_momentaneo, sol, indiceElemento + 1, nodos);
 			valor_candidato -= dp.valores[objetos[indiceElemento]];
-			peso_candidato -= dp.pesos[objetos[indiceElemento]];
+			peso_momentaneo -= dp.pesos[objetos[indiceElemento]];
 		}
 
-		if (cotaSupOpt(objetos, dp, valor_candidato, peso_candidato, indiceElemento +1) > sol)
-			VueltaAtrasFactibilidad(objetos, dp, valor_candidato, peso_candidato, sol, indiceElemento + 1, nodos);
+		if (cotaSupOpt(objetos, dp, valor_candidato, peso_momentaneo, indiceElemento +1) > sol)
+			VueltaAtrasOptimalidad(objetos, dp, valor_candidato, peso_momentaneo, sol, indiceElemento + 1, nodos);
 	}
 }
 
@@ -119,13 +120,14 @@ void VueltaAtrasAmbasPodas(vector<int>const & objetos, DatosProblema const& dp, 
 		if (peso_candidato + dp.pesos[objetos[indiceElemento]] <= dp.tam_Mochila) {
 			valor_candidato += dp.valores[objetos[indiceElemento]];
 			peso_candidato += dp.pesos[objetos[indiceElemento]];
-			VueltaAtrasFactibilidad(objetos, dp, valor_candidato, peso_candidato, sol, indiceElemento + 1, nodos);
+			if (peso_candidato + dp.pesos[objetos[indiceElemento + 1]] <= dp.tam_Mochila)
+				VueltaAtrasAmbasPodas(objetos, dp, valor_candidato, peso_candidato, sol, indiceElemento + 1, nodos);
 			valor_candidato -= dp.valores[objetos[indiceElemento]];
 			peso_candidato -= dp.pesos[objetos[indiceElemento]];
 		}
 
 		if (cotaSupOpt(objetos, dp, valor_candidato, peso_candidato, indiceElemento + 1) > sol)
-			VueltaAtrasFactibilidad(objetos, dp, valor_candidato, peso_candidato, sol, indiceElemento + 1, nodos);
+			VueltaAtrasAmbasPodas(objetos, dp, valor_candidato, peso_candidato, sol, indiceElemento + 1, nodos);
 	}
 }
 
@@ -151,19 +153,19 @@ void pruebaAleatorios(size_t N, ofstream &salida, ofstream &salidaGraficarX, ofs
 	switch (p)
 	{
 	case FACTIBILIDAD:
-		sort(v_indices.begin(), v_indices.end(), [datosProblema](int const & a, const int& b) {
+		sort(v_indices.begin(), v_indices.end(), [datosProblema](int const & a, int const& b) {
 			return (datosProblema.pesos[a] < datosProblema.pesos[b]);
 		});
 		VueltaAtrasFactibilidad(v_indices, datosProblema, 0, 0, valorOptimo, 0, nodos);
 		break;
 	case OPTIMALIDAD:
-		sort(v_indices.begin(), v_indices.end(), [datosProblema](int const & a, const int& b) {
-			return ((datosProblema.valores[a] / datosProblema.pesos[a]) >= (datosProblema.valores[b] / datosProblema.pesos[b]));
+		sort(v_indices.begin(), v_indices.end(), [datosProblema](const int & a, const int& b) {
+			return (datosProblema.valores[a] / datosProblema.pesos[a]) >= (datosProblema.valores[b] / datosProblema.pesos[b]);
 		});
 		VueltaAtrasOptimalidad(v_indices, datosProblema, 0, 0, valorOptimo, 0, nodos);
 		break;
 	case AMBAS:
-		sort(v_indices.begin(), v_indices.end(), [datosProblema](int const & a, const int& b) {
+		sort(v_indices.begin(), v_indices.end(), [datosProblema](int const & a, int const& b) {
 			return ((datosProblema.valores[a] / datosProblema.pesos[a]) >= (datosProblema.valores[b] / datosProblema.pesos[b]));
 		});
 		VueltaAtrasAmbasPodas(v_indices, datosProblema, 0, 0, valorOptimo, 0, nodos);
@@ -188,43 +190,6 @@ void pruebaAleatorios(size_t N, ofstream &salida, ofstream &salidaGraficarX, ofs
 }
 
 
-void pruebaAleatorios(size_t N, ofstream &salida, ofstream &salidaGraficarX, ofstream &salidaGraficarY) {
-	srand(time(NULL));
-	size_t tam_mochila;
-	float valor, peso;
-
-	vector<int> v_indices;
-	DatosProblema datosProblema;
-	datosProblema.tam_Mochila = N;
-	for (size_t i = 0; i < N; ++i) {
-		peso = (RANDOM_MAX - RANDOM_MIN) * ((float)rand() / (float)RAND_MAX) + RANDOM_MIN;
-		valor = (RANDOM_MAX - RANDOM_MIN) * ((float)rand() / (float)RAND_MAX) + RANDOM_MIN;
-		v_indices.push_back(i);
-		datosProblema.pesos.push_back(peso);
-		datosProblema.valores.push_back(valor);
-	}
-	size_t nodos = 0;
-	float valorOptimo;
-	int t0 = clock();
-	sort(v_indices.begin(), v_indices.end(), [datosProblema](int const & a, const int& b) {
-		return (datosProblema.pesos[a] < datosProblema.pesos[b]);
-	});
-	VueltaAtrasFactibilidad(v_indices, datosProblema, 0, 0, valorOptimo, 0, nodos);
-	int t1 = clock();
-	float tiempo = float(t1 - t0) / CLOCKS_PER_SEC;
-	salida << "N = " << N << '\n';
-	salida << "Nodos visitados " << nodos << '\n';
-	salida << "Tiempo requerido = " << tiempo << " segundos" << '\n';
-	salida << "--------------------------\n";
-	salidaGraficarX << N << '\n';
-	salidaGraficarY << fixed;
-	salidaGraficarY << setprecision(3) << tiempo << '\n';
-
-	cout << "N = " << N << '\n';
-	cout << "Tamano de la mochila " << datosProblema.tam_Mochila << '\n';
-	cout << "Nodos visitados " << nodos << '\n';
-	cout << "Tiempo requerido = " << tiempo << " segundos" << '\n';
-}
 
 void pruebaAleatoriosBucleFact() {
 	ofstream salida("SalidaAleatoriosVAtrasFact.txt");
